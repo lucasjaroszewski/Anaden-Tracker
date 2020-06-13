79f0f2c33cfcd0a932f94290106c55339a8d2684
Lucas Jaroszewski <ljnunes@outlook.com.br>
2020-06-13 19:19:33 -0300

    Added Login, Logout, Blog Post-Create-Update-Delete

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='blog-about'),
-    path('blog/', views.blog, name='blog')
+    path('blog/', PostListView.as_view(), name='blog'),
+    path('blog/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('blog/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('blog/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('blog/new/', PostCreateView.as_view(), name='post-create'),
+
 ]
--- dantalion/settings.py
+++ dantalion/settings.py
@@ -123,6 +123,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
 
 django_heroku.settings(locals())
